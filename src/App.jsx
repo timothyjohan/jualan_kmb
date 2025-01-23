9b5d85b1e74a2f423bce063eac6f4c1f1e2ca9eb
Imagine- <simonsugiarta@gmail.com>
2025-01-24 01:55:59 +0700

    fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { useEffect } from 'react'
 import Navbar from './components/Navbar'
@@ -18,6 +18,19 @@ const ProtectedRoute = ({ children }) => {
   return children
 }
 
+// Komponen Layout untuk menangani Navbar
+const Layout = ({ children }) => {
+  const location = useLocation()
+  const isAuthPage = ['/login', '/register'].includes(location.pathname)
+
+  return (
+    <>
+      {!isAuthPage && <Navbar />}
+      {children}
+    </>
+  )
+}
+
 function App() {
   // Tambahkan useEffect untuk set dark mode
   useEffect(() => {
@@ -27,78 +40,77 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Toaster 
-        position="bottom-left"
-        reverseOrder={false}
-        gutter={8}
-        containerClassName=""
-        containerStyle={{}}
-        toastOptions={{
-          // Durasi default 3 detik
-          duration: 3000,
-          
-          // Styling default
-          style: {
-            background: '#1F2937',
-            color: '#fff',
-            padding: '16px',
-            borderRadius: '10px',
-          },
-
-          // Konfigurasi untuk setiap tipe toast
-          success: {
-            style: {
-              background: '#065F46',
-            },
-            iconTheme: {
-              primary: '#fff',
-              secondary: '#065F46',
-            },
-          },
-
-          error: {
+      <Layout>
+        <Toaster 
+          position="bottom-left"
+          reverseOrder={false}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            // Durasi default 3 detik
+            duration: 3000,
+            
+            // Styling default
             style: {
               background: '#1F2937',
+              color: '#fff',
+              padding: '16px',
+              borderRadius: '10px',
             },
-            iconTheme: {
-              primary: '#ef4444',
-              secondary: '#fff',
+
+            // Konfigurasi untuk setiap tipe toast
+            success: {
+              style: {
+                background: '#065F46',
+              },
+              iconTheme: {
+                primary: '#fff',
+                secondary: '#065F46',
+              },
             },
-          },
-        }}
-      />
-      
-      {/* Update kondisi render Navbar */}
-      {!window.location.pathname.match(/\/(login|register)/) && <Navbar />}
-      
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route 
-          path="/kasir" 
-          element={
-            <ProtectedRoute>
-              <KasirPage />
-            </ProtectedRoute>
-          } 
+
+            error: {
+              style: {
+                background: '#1F2937',
+              },
+              iconTheme: {
+                primary: '#ef4444',
+                secondary: '#fff',
+              },
+            },
+          }}
         />
-        <Route 
-          path="/riwayat" 
-          element={
-            <ProtectedRoute>
-              <RiwayatPage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/" 
-          element={
-            <ProtectedRoute>
-              <KasirPage />
-            </ProtectedRoute>
-          } 
-        />
-      </Routes>
+        
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route 
+            path="/kasir" 
+            element={
+              <ProtectedRoute>
+                <KasirPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/riwayat" 
+            element={
+              <ProtectedRoute>
+                <RiwayatPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/" 
+            element={
+              <ProtectedRoute>
+                <KasirPage />
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
